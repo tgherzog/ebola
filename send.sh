53057f6b04f193f1605ce046d73c2433cd913b5c
Tim Herzog <timgherzog@gmail.com>
2014-11-03 16:36:12 +0000

    Replace separate tracking files with a single one

--- send.sh
+++ send.sh
@@ -8,6 +8,8 @@ BASE=`dirname $0`
 SENDER="do-not-reply@zognet.net"
 LOG="$BASE/send_log"
 LOG_DATE="`date "+%D %T"`"
+DATE_FILE="$BASE/last_run"
+TMP="/tmp/send-$$"
 
 # Truncated recipient list, used only in log messages
 LOG_DEST=$DEST
@@ -24,12 +26,12 @@ fi
 # Do case data
 OUTPUT="ebola-cases.xlsx"	# should match the output file from the build script
 
-LAST_RUN=`cat "$BASE/last_run-cases"`
+LAST_RUN=`grep '^cases' $DATE_FILE | cut -d ':' -f 2`
 REPORT=`php "$BASE/cases.php" --cache=0 --status=1`
 THIS_RUN=`echo "$REPORT" | grep "^Most recent date:" | sed -r 's/.+:\\s+//'`
 
 if [ "$LAST_RUN" != "$THIS_RUN" ]; then
-echo $THIS_RUN > "$BASE/last_run-cases"
+sed "s|^cases:.*|cases:$THIS_RUN|" $DATE_FILE > $TMP && mv $TMP $DATE_FILE
 mail -r $SENDER -s "Ebola Case Data as of $THIS_RUN - $OUTPUT" -a "$BASE/data/$OUTPUT" $DEST << END_OF_MAIL
 $REPORT
 END_OF_MAIL
@@ -44,12 +46,12 @@ fi
 # Do price/consumption data
 OUTPUT="ebola-economy.xlsx"	# should match the output file from the build script
 
-LAST_RUN=`cat "$BASE/last_run-econ"`
+LAST_RUN=`grep '^econ' $DATE_FILE | cut -d ':' -f 2`
 THIS_RUN=`php "$BASE/econ.php" --cache=0 --status=date`
 
 
 if [ "$LAST_RUN" != "$THIS_RUN" ]; then
-echo $THIS_RUN > "$BASE/last_run-econ"
+sed "s|^econ:.*|econ:$THIS_RUN|" $DATE_FILE > $TMP && mv $TMP $DATE_FILE
 mail -r $SENDER -s "Ebola Market Data as of $THIS_RUN - $OUTPUT" -a "$BASE/data/$OUTPUT" $DEST << END_OF_MAIL
 New case data posted: $THIS_RUN
 
@@ -64,11 +66,11 @@ fi
 
 # WFP indicators
 OUTPUT="ebola-wfp.xlsx"	# should match the output file from the build script
-LAST_RUN=`cat "$BASE/last_run-wfp"`
+LAST_RUN=`grep '^wfp' $DATE_FILE | cut -d ':' -f 2`
 REPORT=`php "$BASE/wfp.php" --cache=0 --status=1`
 THIS_RUN=`echo "$REPORT" | grep "^Most recent date:" | sed -r 's/.+:\\s+//'`
 if [ "$LAST_RUN" != "$THIS_RUN" ]; then
-echo $THIS_RUN > "$BASE/last_run-wfp"
+sed "s|^wfp:.*|wfp:$THIS_RUN|" $DATE_FILE > $TMP && mv $TMP $DATE_FILE
 mail -r $SENDER -s "Ebola WFP Data as of $THIS_RUN - $OUTPUT" -a "$BASE/data/$OUTPUT" $DEST << END_OF_MAIL
 $REPORT
 END_OF_MAIL
